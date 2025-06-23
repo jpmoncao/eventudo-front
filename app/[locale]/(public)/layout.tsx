f7a27ea9e70431082e9e756dde6a192bd0fc56be
jpmoncao <pedrohjoao44@gmail.com>
2025-06-23 17:12:29 -0300

    feat: add scrollarea to main container

--- app/[locale]/(public)/layout.tsx
+++ app/[locale]/(public)/layout.tsx
@@ -1,16 +1,29 @@
 import Header from "./_components/header";
 
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+
 export default function PublicLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <>
-      <Header />
-      <main className="container min-h-screen px-4 pt-4 bg-background text-foreground overflow-x-hidden max-w-[1000px] mx-auto">
-        {children}
-      </main>
-    </>
+    <div className="flex flex-col w-full h-dvh">
+      <ScrollArea className="h-dvh">
+        <Header />
+        
+        <main className="container min-h-screen px-4 pt-4 bg-background text-foreground overflow-x-hidden max-w-[1000px] mx-auto">
+          {children}
+        </main>
+
+        <footer className="container px-4 py-2 text-sm text-muted-foreground">
+          <p className="text-center">
+            © {new Date().getFullYear()} Eventudo. All rights reserved.
+          </p>
+        </footer>
+
+        <ScrollBar />
+      </ScrollArea>
+    </div>
   );
 }
--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -26,7 +26,7 @@ export default async function RootLayout({
   params
 }: {
   children: React.ReactNode;
-  params: Promise<{ locale: string }>;
+  params: { locale: string };
 }) {
   const { locale } = await params;
   if (!hasLocale(routing.locales, locale)) {
