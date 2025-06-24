1fe46ac87fc774bea0171362a748ad5a5b96450f
jpmoncao <pedrohjoao44@gmail.com>
2025-06-24 09:39:05 -0300

    feat: add hero section to home

---
+++ app/[locale]/(public)/_sections/hero.tsx
@@ -0,0 +1,24 @@
+import { Button } from "@/components/ui/button";
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+
+export default function HeroSection() {
+    const t = useTranslations("");
+
+    return (
+        <section>
+            <h1 className="text-4xl font-bold font-heading text-center">{t("pages.home.hero.title")} <strong className="text-primary">{t("globals.appName")}!</strong></h1>
+            <p className="mt-4 text-lg text-center">{t("pages.home.hero.description")}</p>
+            <Image
+                src="/images/hero.svg"
+                alt={t("pages.home.hero.imageAlt")}
+                width={500}
+                height={300}
+                className="mx-auto mt-8 drop-shadow-2xl"
+            />
+            <div className="mt-6 w-full flex justify-center">
+                <Button className="cta-gradient">{t("pages.home.hero.cta")}</Button>
+            </div>
+        </section>
+    )
+}
--- app/[locale]/(public)/page.tsx
+++ app/[locale]/(public)/page.tsx
@@ -1,12 +1,14 @@
 import { useTranslations } from "next-intl";
 
+// Section imports
+import HeroSection from "./_sections/hero";
+
 export default function HomePage() {
   const t = useTranslations("");
 
   return (
     <>
-      <h1 className="text-4xl font-bold font-heading text-center">{t("pages.home.title")} <strong className="text-primary">{t("globals.appName")}!</strong></h1>
-      <p className="mt-4 text-lg text-center">{t("pages.home.description")}</p>
+      <HeroSection />
     </>
   );
 }
